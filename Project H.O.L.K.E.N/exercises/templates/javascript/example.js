b692e9311ab64267da4e88bcdc6d17177531afa8
Maximilian Hallqvist <98323670+NoLifer47@users.noreply.github.com>
2022-05-10 14:45:26 +0200

    No1

---
+++ Project H.O.L.K.E.N/exercises/templates/javascript/example.js
@@ -0,0 +1,10 @@
+function visaDialog(){
+    alert('Hej på dej!');
+    changeText();
+}
+
+function changeText(){}
+document.getElementById("demo").innerHTML = "Hello  javascript;";function visaDialog(){
+    alert('hehe noob');
+    changeText();
+}
