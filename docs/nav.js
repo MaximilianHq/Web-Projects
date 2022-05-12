c84eb72c0c1d41aa7f079bbde9e8749256c8b1e0
Maximilian Hallqvist <98323670+NoLifer47@users.noreply.github.com>
2022-05-12 19:50:04 +0200

    Save 2.0

--- docs/nav.js
+++ docs/nav.js
@@ -2,6 +2,7 @@
 function openNav() {
     document.getElementById("nav").style.width = "300px";
 }
+
 function closeNav() {
     document.getElementById("nav").style.width = "75px";
 }
@@ -10,6 +11,7 @@ function closeNav() {
 function openNavProjects() {
     document.getElementById("navProjects").style.width = "300px";
 }
+
 function closeNavProjects() {
     document.getElementById("navProjects").style.width = "0px";
 }
@@ -18,6 +20,7 @@ function closeNavProjects() {
 function openNavSites() {
     document.getElementById("navSites").style.width = "300px";
 }
+
 function closeNavSites() {
     document.getElementById("navSites").style.width = "0px";
 }
@@ -26,6 +29,7 @@ function closeNavSites() {
 function openNavResourcepacks() {
     document.getElementById("navResourcepacks").style.width = "300px";
 }
+
 function closeNavResourcepacks() {
     document.getElementById("navResourcepacks").style.width = "0px";
 }
@@ -33,29 +37,32 @@ function closeNavResourcepacks() {
 
 // EXERCISES NAVIGATION
 
-    // EXERCISES NAVIGATION MENU
-    function openNavExercises() {
-        document.getElementById("navExercises").style.width = "300px";
-    }
-    function closeNavExercises() {
-        document.getElementById("navExercises").style.width = "0px";
-    }
+// EXERCISES NAVIGATION MENU
+function openNavExercises() {
+    document.getElementById("navExercises").style.width = "300px";
+}
 
-        // PRACTICES NAVIGATION MENU
-        function openNavPractices() {
-            document.getElementById("navPractices").style.width = "300px";
-        }
-        function closeNavPractices() {
-            document.getElementById("navPractices").style.width = "0px";
-        }
+function closeNavExercises() {
+    document.getElementById("navExercises").style.width = "0px";
+}
 
-        // TEMPLATES NAVIGATION MENU
-        function openNavTemplates() {
-            document.getElementById("navTemplates").style.width = "300px";
-        }
-        function closeNavTemplates() {
-            document.getElementById("navTemplates").style.width = "0px";
-        }
+// PRACTICES NAVIGATION MENU
+function openNavPractices() {
+    document.getElementById("navPractices").style.width = "300px";
+}
+
+function closeNavPractices() {
+    document.getElementById("navPractices").style.width = "0px";
+}
+
+// TEMPLATES NAVIGATION MENU
+function openNavTemplates() {
+    document.getElementById("navTemplates").style.width = "300px";
+}
+
+function closeNavTemplates() {
+    document.getElementById("navTemplates").style.width = "0px";
+}
 
 // EXERCISES NAVIGATION
 
